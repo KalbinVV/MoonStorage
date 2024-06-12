5327e2305b90a8bec53148c5682b392c34d75fe4
Kalbin Vladimir <kalbin.wow@yandex.ru>
2024-06-12 17:45:10 +0500

    Move from couchdb to postgres

--- templates/couchdb-initializer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sleep 15
-curl -X PUT http://{couchdb_admin_username}:{couchdb_admin_password}@couchdb:{couchdb_port}/_users 
-curl -X PUT http://{couchdb_admin_username}:{couchdb_admin_password}@couchdb:{couchdb_port}/_replicator 
-curl -X PUT http://{couchdb_admin_username}:{couchdb_admin_password}@couchdb:{couchdb_port}/_global_changes
--- templates/ipfs-scripts/ipfs-node-init.sh
+++ templates/ipfs-scripts/ipfs-node-init.sh
@@ -2,7 +2,3 @@
 
 set -ex
 ipfs bootstrap rm all
-
-peer_id="`ipfs config Identity.PeerID`"
-
-ipfs bootstrap add "/ip4/{ipfs_peer_id_addr}/tcp/4001/ipfs/$peer_id"
